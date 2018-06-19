53e32ab8969a307c942690a7916e8abc082219a0
JOBFER <gbringashotmail@hotmail.com>
2018-06-19 14:20:47 +0200

    Inicio

---
+++ src/java/dao/CategoriaFacade.java
@@ -0,0 +1,59 @@
+package dao;
+
+import entities.Categoria;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.Resource;
+import javax.ejb.EJB;
+import javax.ejb.EJBContext;
+import javax.ejb.Stateless;
+import javax.ejb.TransactionManagement;
+import javax.ejb.TransactionManagementType;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.PersistenceContext;
+import javax.transaction.HeuristicMixedException;
+import javax.transaction.HeuristicRollbackException;
+import javax.transaction.NotSupportedException;
+import javax.transaction.RollbackException;
+import javax.transaction.SystemException;
+import javax.transaction.UserTransaction;
+
+//Como se usa "UserTransaction", solo se permite BEAN. Da error si se pone CONTAINER o si se deja en blanco
+@TransactionManagement(TransactionManagementType.BEAN)
+@Stateless
+public class CategoriaFacade extends AbstractFacade<Categoria> {
+
+    @EJB
+    private EJBSubordinado eJBSubordinado;
+
+    @Resource
+    private EJBContext context;
+
+    @PersistenceContext(unitName = "EjemploBD_JTA_JNDI_BMTPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public CategoriaFacade() {
+        super(Categoria.class);
+    }
+
+    public void crearCategoria() throws SystemException{
+        System.out.println("*************** principal ****************");
+        UserTransaction tx = context.getUserTransaction();
+        try {
+            tx.begin();
+            em.persist(new Categoria("miCategoria"));
+//            em.persist(new Categoria(1)); //Para que se produzca una excepcion
+            eJBSubordinado.subordinado();
+            tx.commit();            
+            
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }        
+    }
+}
---
+++ src/java/dao/EJBSubordinado.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import entities.Categoria;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.Resource;
+import javax.ejb.EJBContext;
+import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.ejb.TransactionManagement;
+import javax.ejb.TransactionManagementType;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import javax.persistence.PersistenceContext;
+import javax.transaction.NotSupportedException;
+import javax.transaction.SystemException;
+import javax.transaction.UserTransaction;
+
+@Stateless
+@TransactionManagement(TransactionManagementType.BEAN)
+public class EJBSubordinado {
+    
+    @Resource
+    private EJBContext context;    
+    
+    @PersistenceContext(unitName = "EjemploBD_JTA_JNDI_BMTPU")
+    private EntityManager em;
+    
+    //Con BEAN no se permite ningun "TransactionAttributeType". Da error de compilacion.
+//    @TransactionAttribute(value = TransactionAttributeType.NEVER)
+//    @TransactionAttribute(value = TransactionAttributeType.REQUIRES_NEW)
+    public void subordinado() throws Exception {
+        System.out.println("*************** subordinado ****************");
+                
+        UserTransaction tx = context.getUserTransaction();
+        tx.begin();
+        em.persist(new Categoria("miCategoriaSubordinada"));
+        tx.commit();            
+    }
+}
